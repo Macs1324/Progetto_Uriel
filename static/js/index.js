682bf30f3466f859ec7bcdea15f88ef2a53eadbb
Macs1324 <uzzoloh@gmail.com>
2021-04-13 22:28:19 +0200

    Added transitions and started implementing slideshow

--- static/js/index.js
+++ static/js/index.js
@@ -1,46 +1,65 @@
+
+//--------------------------------------------------------------------THE SHIT NEEDED FOR SCROLL DETECTION
 // The debounce function receives our function as a parameter
-const debounce = (fn) => {
+  const debounce = (fn) => {
 
-    // This holds the requestAnimationFrame reference, so we can cancel it if we wish
-    let frame;
+      // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+      let frame;
 
-    // The debounce function returns a new function that can receive a variable number of arguments
-    return (...params) => {
-      
-        // If the frame variable has been defined, clear it now, and queue for next frame
-        if (frame) { 
-          cancelAnimationFrame(frame);
-        }
+      // The debounce function returns a new function that can receive a variable number of arguments
+      return (...params) => {
+        
+          // If the frame variable has been defined, clear it now, and queue for next frame
+          if (frame) { 
+            cancelAnimationFrame(frame);
+          }
 
-        // Queue our function call for the next frame
-        frame = requestAnimationFrame(() => {
-          
-          // Call our function and pass any params we received
-          fn(...params);
-        });
+          // Queue our function call for the next frame
+          frame = requestAnimationFrame(() => {
+            
+            // Call our function and pass any params we received
+            fn(...params);
+          });
 
-    } 
-  };
+      } 
+    };
 
 
-  // Reads out the scroll position and stores it in the data attribute
-  // so we can use it in our stylesheets
+    // Reads out the scroll position and stores it in the data attribute
+    // so we can use it in our stylesheets
   const storeScroll = () => {
-    document.documentElement.dataset.scroll = window.scrollY;
-    
-    //Shades the header if it's sticking to the top
-    if(window.scrollY > 2)
-    {
-        document.getElementsByClassName("header")[0].style.boxShadow = "0px 1em 0.5em 0.1em rgba(0, 0, 0, 0.6)";
+      document.documentElement.dataset.scroll = window.scrollY;
+      
+      //Shades the header if it's sticking to the top
+      if(window.scrollY > 1)
+      {
+          document.getElementsByClassName("header")[0].style.boxShadow = "0px 1em 0.5em 0.1em rgba(0, 0, 0, 0.6)";
+          document.getElementsByClassName("header")[0].getElementsByClassName("logo")[0].getElementsByTagName("img")[0].style.width = "15%";
+      }
+      else
+      {
+          document.getElementsByClassName("header")[0].style.boxShadow = "none";
+          document.getElementsByClassName("header")[0].getElementsByClassName("logo")[0].getElementsByTagName("img")[0].style.width = "20%";
+      }
     }
-    else
-    {
-        document.getElementsByClassName("header")[0].style.boxShadow = "none";
-    }
-  }
 
-  // Listen for new scroll events, here we debounce our `storeScroll` function
-  document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+    // Listen for new scroll events, here we debounce our `storeScroll` function
+    document.addEventListener('scroll', debounce(storeScroll), { passive: true });
 
-  // Update scroll position for first time
-  storeScroll();
+    // Update scroll position for first time
+    storeScroll();
+
+
+//-----------------------------------------------------------------------SLIDESHOW
+function slideshow_view(index)
+{
+	let images = document.getElementsByClassName("slideshow")[0].getElementsByClassName("item");
+	let buttons = document.getElementsByClassName("slideshow-container")[0].getElementsByClassName("buttons")[0].getElementsByClassName("dot");
+	for(let i = 0; i < images.length; i++)
+	{
+		images[i].style.display = "none";
+		buttons[i].style.backgroundColor = "#bbb";
+	}
+	images[index].style.display = "block";
+	buttons[index].style.backgroundColor = "#666";
+}
